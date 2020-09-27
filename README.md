aac788cf16473f7309039479e4b579ef9d01dd7f
mayrazan <mayramanchein@yahoo.com.br>
2020-09-27 14:14:24 -0300

    Implementando as funções buscar e editar

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 Objetivo: Implementar um programa em python para uma agenda telefônica digital, seguindo os seguintes critérios:
 
 • Menu: Deve ter as seguintes opções para o usuário:
+
 ◦ 1 - Inserir contato
+
 ◦ 2 - Editar contato
+
 ◦ 3 - Excluir contato
+
 ◦ 4 - Exibir contatos
+
 ◦ 5 – Buscar contato
+
 ◦ 0 – Sair
 
 • Dados: Deve armazenar em uma lista, contendo os dados:
--- agenda.py
+++ agenda.py
@@ -17,12 +17,73 @@ class Agenda:
         agenda.append(lista)
 
     def editar(self):
-        print("b")
+        print("Selecione uma opção: ")
+        print("1. Editar contato por nome: ")
+        print("2. Editar contato por telefone: ")
+        opcao = input("> ")
+
+        if opcao == "1":
+            nome = input("Digite o nome: ")
+            for posicao, i in enumerate(agenda): 
+                for j in i:
+                    if nome == j:
+                        print("Selecione a opção que você quer editar: ")
+                        print("1. Editar nome: ")
+                        print("2. Editar e-mail: ")
+                        print("3. Editar idade: ")
+                        print("4. Editar telefone: ")
+                        op = input("> ")
+
+                        if op == "1":
+                            a = input("Novo nome: ")
+                            agenda[posicao][0] = a
+                            break
+                        elif op == "2":
+                            b = input("Novo e-mail: ")
+                            agenda[posicao][1] = b  
+                            break
+                        elif op == "3":
+                            c = input("Nova idade: ")
+                            agenda[posicao][2] = c
+                            break
+                        elif op == "4":
+                            d = input("Novo telefone: ")
+                            agenda[posicao][3] = d
+                            break
+
+        elif opcao == "2":
+            telefone = input("Digite o telefone: ")
+            for posicao, i in enumerate(agenda): 
+                for j in i:
+                    if telefone == j:
+                        print("Selecione a opção que você quer editar: ")
+                        print("1. Editar nome: ")
+                        print("2. Editar e-mail: ")
+                        print("3. Editar idade: ")
+                        print("4. Editar telefone: ")
+                        op = input("> ")
+
+                        if op == "1":
+                            a = input("Novo nome: ")
+                            agenda[posicao][0] = a
+                            break
+                        elif op == "2":
+                            b = input("Novo e-mail: ")
+                            agenda[posicao][1] = b  
+                            break
+                        elif op == "3":
+                            c = input("Nova idade: ")
+                            agenda[posicao][2] = c
+                            break
+                        elif op == "4":
+                            d = input("Novo telefone: ")
+                            agenda[posicao][3] = d
+                            break
 
     def excluir(self):
         print("Selecione uma opção: ")
-        print("1. Excluir por nome: ")
-        print("2. Excluir por telefone: ")
+        print("1. Excluir contato por nome: ")
+        print("2. Excluir contato por telefone: ")
         opcao = input("> ")
 
         if opcao == "1":
@@ -31,16 +92,39 @@ class Agenda:
                 for j in i:
                     if nome == j:
                         agenda.pop(posicao)
+                        break
         elif opcao == "2":
             telefone = input("Digite o telefone: ")
             for posicao, i in enumerate(agenda):
                 for j in i:
                     if telefone == j:
-                        agenda.pop(posicao)    
+                        agenda.pop(posicao)  
+                        break 
 
     def exibir(self):
-        print(agenda)
+        if len(agenda) == 0:
+            print("Agenda vazia.")
+        else:
+            print(agenda)
 
     def buscar(self):
-        print("e")
+        print("Selecione uma opção: ")
+        print("1. Buscar contato por nome: ")
+        print("2. Buscar contato por telefone: ")
+        opcao = input("> ")
+
+        if opcao == "1":
+            nome = input("Digite o nome: ")
+            for posicao, i in enumerate(agenda): 
+                for j in i:
+                    if nome == j:
+                        print(i)
+                        break
+        elif opcao == "2":
+            telefone = input("Digite o telefone: ")
+            for posicao, i in enumerate(agenda): 
+                for j in i:
+                    if telefone == j:
+                        print(i)
+                        break  
 
